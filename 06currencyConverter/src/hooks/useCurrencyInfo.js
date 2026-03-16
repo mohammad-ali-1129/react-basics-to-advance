4c9c20216dc9e72e32fa417df6a2a937347d8cf1
Mohammad Ali <aliuser1129@gmail.com>
2026-03-16 16:21:29 +0530

    Created a currency converter and learnt about fetch api

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({})
-    useEffect(() => {
-        fetch( `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
-    }, [currency])
-    console.log(data)
-    return data
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.rates || {});
+      })
+      .catch(() => {
+        setData({});
+      });
+  }, [currency]);
+
+  return data;
 }
 
-
-export default useCurrencyInfo
+export default useCurrencyInfo;
