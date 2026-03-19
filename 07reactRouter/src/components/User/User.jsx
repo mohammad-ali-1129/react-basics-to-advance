f2bf251742d0b06b5370e1c55610a6e522b7c961
Mohammad Ali <aliuser1129@gmail.com>
2026-03-19 18:16:38 +0530

    Learnt about react douter dom in detail and made project fetching github api

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 function User() {
+
+  
+  const {userid} = useParams()
   return (
-    <div>
-      <h1>User</h1>
-      <p>Welcome to the User page</p>
-    </div>
+    <div className="bg-gray-600 text-white text-3xl p-4">User : {userid}</div>
   );
 }
 
