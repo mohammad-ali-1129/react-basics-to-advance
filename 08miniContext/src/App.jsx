d753e35c69f21e87712c8c326fba81723aaf9e60
Mohammad Ali <aliuser1129@gmail.com>
2026-03-22 17:27:16 +0530

    Built basic login components using context api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Ali</h1>
-    </>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
